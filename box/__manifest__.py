44bb741c4afa7a630d8ed3b02f07c053a28b40d6
Juan P Garza <juanp@juanpgarza.com>
2020-02-22 15:33:14 -0300

    [BUG] faltaba módulo en depends

--- box/__manifest__.py
+++ box/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','point_of_sale'],
+    'depends': ['base','account','account_payment_group'],
 
     # always loaded
     'data': [
