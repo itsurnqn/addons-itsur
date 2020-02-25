020284bb68335251ef79b9f836eb748245b76a74
Juan P Garza <juanp@juanpgarza.com>
2020-02-25 18:30:22 -0300

    avances

--- box/__manifest__.py
+++ box/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','account_payment_group'],
+    'depends': ['base','account','account_payment_group','account_payment_credit_card_mgmt'],
 
     # always loaded
     'data': [
