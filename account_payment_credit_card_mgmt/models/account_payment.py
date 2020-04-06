ac8c9a430868440039f659b655f298e4e21823d7
Juan P Garza <juanp@juanpgarza.com>
2020-04-06 17:10:48 -0300

    avances

--- account_payment_credit_card_mgmt/models/account_payment.py
+++ account_payment_credit_card_mgmt/models/account_payment.py
@@ -5,9 +5,9 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
-class AccountPaymentTipoTarjeta(models.Model):
-	_name = 'account.payment.tipo.tarjeta'
-	_description = 'Tipo de tarjeta de credito'
+class AccountPaymentPlanTarjeta(models.Model):
+	_name = 'account.payment.plan.tarjeta'
+	_description = 'Plan de tarjeta de credito'
 
 	name = fields.Char('Nombre')
 
@@ -18,11 +18,13 @@ class AccountPayment(models.Model):
     nro_cupon = fields.Char('Nro Cupon')
     nro_tarjeta = fields.Char('Nro Tarjeta')
     cant_cuotas = fields.Integer('Cuotas')
-    tipo_tarjeta_id = fields.Many2one('account.payment.tipo.tarjeta',string='Tipo Tarjeta', ondelete='Restrict')
+    plan_tarjeta_id = fields.Many2one('account.payment.plan.tarjeta',string='Plan Tarjeta', ondelete='Restrict')
+    nro_lote = fields.Integer('Nro lote')
 
 	# @api.one
-    @api.constrains('nro_cupon','nro_tarjeta','tipo_tarjeta_id')
+    # @api.constrains('nro_cupon','nro_tarjeta','plan_tarjeta_id')
+    @api.constrains('nro_cupon','nro_lote')
     def _check_pago_tarjeta(self):
         if self.is_credit_card:
-            if (not self.nro_cupon) or (not self.nro_tarjeta) or (not self.tipo_tarjeta_id):
+            if (not self.nro_cupon) or (not self.nro_lote) or (not self.plan_tarjeta_id):
                 raise ValidationError("Debe ingresar los datos de pago de tarjeta")
