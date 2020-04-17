c985b3ee86012bcd82b654a2bedf6bf7a538f19f
Juan P Garza <juanp@juanpgarza.com>
2020-04-17 17:52:04 -0300

    correcciones caja

--- box/models/account_payment.py
+++ box/models/account_payment.py
@@ -10,6 +10,8 @@ class AccountPayment(models.Model):
 
     box_session_id = fields.Many2one('box.session', string='Sesión de caja', readonly="True", ondelete='Restrict', domain="[('state', '=', 'opened'),('user_id', '=', uid)]")
 
+    box_session_journal_line_id = fields.Many2one('box.session.journal.line')
+
     @api.multi
     def get_journals_domain(self):
         domain = super(AccountPayment, self).get_journals_domain()
@@ -24,7 +26,6 @@ class AccountPayment(models.Model):
 
     @api.multi
     def cancel(self):
-        # import pdb; pdb.set_trace()
         super(AccountPayment,self).cancel()
 
         # tengo que controlar que la caja este abierta.
@@ -33,27 +34,43 @@ class AccountPayment(models.Model):
         if not session_actual:
             raise UserError(_("Debe iniciar una sesión de caja para poder cancelar el recibo."))
         else:
-            # raise UserError(_("TODO OK."))
-            # import pdb; pdb.set_trace()
-            # renglon_caja = self.env['box.session.journal.line'].browse(self.id)
-            renglon_caja = self.env['box.session.journal.line'].search([('account_payment_id','=',self.id)])
-            
-            # analizar que pasa cuando se está anulando un recibo de un turno anterior ...
-            # me tengo que asegurar que se registre en la sesión actual            
-            # busco el diario que corresponda según lo que se este anulando (self.journal_id)            
-            # busco la linea (box.session.journal) que corresponde para el diario, dentro de la sesión actual. 
-            
-            box_session_journal_id = self.env['box.session.journal'].search([('journal_id','=',self.journal_id.id),('box_session_id.id','=',session_actual.id)])
+            # si se cancela antes de validar el recibo, puede pasar que no tenga renglon de pago (para recibos y para OP)
+            # en este caso self.id = 0. entonces solo debo cancelar la linea de caja en caso que exista el renglón de pago
+            if (self.id != 0):
+                # solo si el recibo está validado se tiene que anular en la caja
+                if (self.payment_group_id.state == 'posted'):
 
-            # y me quedo con el id 
+                    print("Anula en la caja")
 
-            vals = {
-                'name': renglon_caja.display_name, 
-                'amount': -renglon_caja.amount, 
-                'partner_id': renglon_caja.partner_id.id,
-                'ref': renglon_caja.ref + ' - Cancelación',
-                'account_payment_id': renglon_caja.account_payment_id.id,
-                'box_session_journal_id': box_session_journal_id.id
-            }
+                    # renglon_caja = self.env['box.session.journal.line'].search([('account_payment_id','=',self.id)])
+                    renglon_caja = self.env['box.session.journal.line'].browse(self.box_session_journal_line_id.id)
 
-            self.env['box.session.journal.line'].create(vals)  
+                    # analizar que pasa cuando se está anulando un recibo de un turno anterior ...
+                    # me tengo que asegurar que se registre en la sesión actual            
+                    # busco el diario que corresponda según lo que se este anulando (self.journal_id)            
+                    # busco la linea (box.session.journal) que corresponde para el diario, dentro de la sesión actual. 
+                    
+                    box_session_journal_id = self.env['box.session.journal'].search([('journal_id','=',self.journal_id.id),('box_session_id.id','=',session_actual.id)])
+                    # box_session_journal_id = self.env['box.session.journal'].browse(self.box_session_journal_line_id.box_session_journal_id.id)
+
+                    # import pdb; pdb.set_trace()
+
+                    if (renglon_caja.ref):
+                        ref = renglon_caja.ref
+                    else:
+                        ref = ''
+
+                    vals = {
+                        'name': renglon_caja.display_name, 
+                        'amount': -renglon_caja.amount, 
+                        'partner_id': renglon_caja.partner_id.id,
+                        'ref': ref + ' - Cancelación',
+                        'account_payment_id': renglon_caja.account_payment_id.id,
+                        'box_session_journal_id': box_session_journal_id.id
+                    }
+
+                    self.env['box.session.journal.line'].create(vals)
+                else:
+                    print("No tiene que anular en la caja 1.")
+            else:
+                print("No tiene que anular en la caja 2.")
--- box/models/account_payment_group.py
+++ box/models/account_payment_group.py
@@ -25,56 +25,64 @@ class AccountPaymentGroup(models.Model):
     def post(self):
         super(AccountPaymentGroup, self).post()
         for rec in self:
-            for rec2 in rec.payment_ids.filtered(lambda x: x.state == 'posted'):
-                rec2.box_session_id = self.box_session_id
+            
+            # verificar que la sesión de caja esté abierta.
+            # ej para los casos en los que el recibo quedó en borrador con una sesión antigua
+            
+            if (rec.box_session_id.state != 'opened'):
+                raise UserError(_("La sesión de caja esta cerrada. %s") % rec.box_session_id.display_name)
+            else:
+                for rec2 in rec.payment_ids.filtered(lambda x: x.state == 'posted'):
+                    rec2.box_session_id = self.box_session_id
 
-                box_session_journal_id = self.env['box.session.journal'].search(['&',('box_session_id','=',self.box_session_id.id),('journal_id','=',rec2.journal_id.id)])
-                #import pdb;pdb.set_trace()
+                    box_session_journal_id = self.env['box.session.journal'].search(['&',('box_session_id','=',self.box_session_id.id),('journal_id','=',rec2.journal_id.id)])
+                    #import pdb;pdb.set_trace()
 
-                
+                    
 
-                if self.partner_type == 'supplier':
-                    amount = - rec2.amount
+                    if self.partner_type == 'supplier':
+                        amount = - rec2.amount
 
-                    ref = 'pago a proveedor'
+                        ref = 'pago a proveedor'
 
-                else:
-                    amount = rec2.amount                
-
-                    inbound_payment_method_codes = rec2.journal_id.inbound_payment_method_ids.mapped('code')
-
-                    if 'received_third_check' in inbound_payment_method_codes:
-                        # cheque de tercero
-                        ref = rec2.check_bank_id.name + ' - ' + rec2.check_payment_date.strftime("%m/%d/%Y")
-                    elif 'electronic' in inbound_payment_method_codes:
-                        # transferencia bancaria
-                        ref = 'Transferencia '
-                    elif 'withholding' in inbound_payment_method_codes:
-                        # retenciones
-                        ref = rec2.tax_withholding_id.name + ' - ' + rec2.withholding_number
-                    elif 'inbound_credit_card' in inbound_payment_method_codes:
-                        # tarjeta crédito
-                        ref = "Lote: " + str(rec2.nro_lote) + " - " + "Cupón: " + rec2.nro_cupon
-                    elif 'outbound_debit_card' in inbound_payment_method_codes:
-                        # tarjeta débito
-                        ref = ''
                     else:
-                        ref = ''
+                        amount = rec2.amount                
 
-                    # if rec2.journal_id.type == 'bank':                        
-                    # else:
-                    #     ref = rec2.display_name
+                        inbound_payment_method_codes = rec2.journal_id.inbound_payment_method_ids.mapped('code')
 
-                if rec2.communication != '.' and rec2.communication:
-                    ref = ref + ' - ' + rec2.communication
+                        if 'received_third_check' in inbound_payment_method_codes:
+                            # cheque de tercero
+                            ref = rec2.check_bank_id.name + ' - ' + rec2.check_payment_date.strftime("%m/%d/%Y")
+                        elif 'electronic' in inbound_payment_method_codes:
+                            # transferencia bancaria
+                            ref = 'Transferencia '
+                        elif 'withholding' in inbound_payment_method_codes:
+                            # retenciones
+                            ref = rec2.tax_withholding_id.name + ' - ' + rec2.withholding_number
+                        elif 'inbound_credit_card' in inbound_payment_method_codes:
+                            # tarjeta crédito
+                            ref = "Lote: " + str(rec2.nro_lote) + " - " + "Cupón: " + rec2.nro_cupon
+                        elif 'outbound_debit_card' in inbound_payment_method_codes:
+                            # tarjeta débito
+                            ref = ''
+                        else:
+                            ref = ''
 
-                vals = {
-                    'name': rec2.display_name, 
-                    'amount': amount, 
-                    'partner_id': self.partner_id.id,
-                    'ref': ref,
-                    'account_payment_id': rec2.id,
-                    'box_session_journal_id': box_session_journal_id.id
-                }
+                        # if rec2.journal_id.type == 'bank':                        
+                        # else:
+                        #     ref = rec2.display_name
 
-                self.env['box.session.journal.line'].create(vals)
+                    if rec2.communication != '.' and rec2.communication:
+                        ref = ref + ' - ' + rec2.communication
+
+                    vals = {
+                        'name': rec2.display_name, 
+                        'amount': amount, 
+                        'partner_id': self.partner_id.id,
+                        'ref': ref,
+                        'account_payment_id': rec2.id,
+                        'box_session_journal_id': box_session_journal_id.id
+                    }                  
+                    
+                    rec2.box_session_journal_line_id = self.env['box.session.journal.line'].create(vals)
+                    # import pdb; pdb.set_trace()
