50b6258a7c426cf40e2d4393b09a4d880778cca9
Juan P Garza <juanp@juanpgarza.com>
2020-04-27 20:28:16 -0300

    [BUG] Fallaba cuando se intenta cancelar un recibo con más de un medio de pago

--- box/models/account_payment.py
+++ box/models/account_payment.py
@@ -36,41 +36,48 @@ class AccountPayment(models.Model):
         else:
             # si se cancela antes de validar el recibo, puede pasar que no tenga renglon de pago (para recibos y para OP)
             # en este caso self.id = 0. entonces solo debo cancelar la linea de caja en caso que exista el renglón de pago
-            if (self.id != 0):
-                # solo si el recibo está validado se tiene que anular en la caja
-                if (self.payment_group_id.state == 'posted'):
 
-                    print("Anula en la caja")
+            # el metodo cancel se invoca desde el metodo cancel del account_payment_group de adhoc.
+            # y lo invoca así: rec.payment_ids.cancel() . eso quiere decir que como self de este metodo recibe
+            # algo del tipo account_payment(). que es así account_payment(n) si el recibo tiene n medios de pago.
+               
+            # si no tiene renglones, no hay nada que anular en la caja
+            if len(self) > 0:
+                
+                for rec in self:
+                    # solo si el renglon está validado se tiene que anular en la caja
+                    if (rec.payment_group_id.state == 'posted'):
 
-                    # renglon_caja = self.env['box.session.journal.line'].search([('account_payment_id','=',self.id)])
-                    renglon_caja = self.env['box.session.journal.line'].browse(self.box_session_journal_line_id.id)
+                        print("Anula en la caja")
 
-                    # analizar que pasa cuando se está anulando un recibo de un turno anterior ...
-                    # me tengo que asegurar que se registre en la sesión actual            
-                    # busco el diario que corresponda según lo que se este anulando (self.journal_id)            
-                    # busco la linea (box.session.journal) que corresponde para el diario, dentro de la sesión actual. 
-                    
-                    box_session_journal_id = self.env['box.session.journal'].search([('journal_id','=',self.journal_id.id),('box_session_id.id','=',session_actual.id)])
-                    # box_session_journal_id = self.env['box.session.journal'].browse(self.box_session_journal_line_id.box_session_journal_id.id)
+                        # renglon de la caja que tengo que anular
+                        renglon_caja = self.env['box.session.journal.line'].browse(rec.box_session_journal_line_id.id)
 
-                    # import pdb; pdb.set_trace()
+                        # analizar que pasa cuando se está anulando un recibo de un turno anterior ...
+                        # me tengo que asegurar que se registre en la sesión actual            
+                        # busco el diario que corresponda según lo que se este anulando (self.journal_id)            
+                        # busco la linea (box.session.journal) que corresponde para el diario, dentro de la sesión actual.                         
+                        box_session_journal_id = self.env['box.session.journal'].search([('journal_id','=',rec.journal_id.id),('box_session_id.id','=',session_actual.id)])
 
-                    if (renglon_caja.ref):
-                        ref = renglon_caja.ref
+                        # Cuidado!! que paso si la sesion actual ahora no tiene el medio de pago que estoy anulando? en la sesión original lo tenía pero ahora no.
+
+                        if (renglon_caja.ref):
+                            ref = renglon_caja.ref
+                        else:
+                            ref = ''
+
+                        vals = {
+                            'name': renglon_caja.display_name, 
+                            'amount': -renglon_caja.amount, 
+                            'partner_id': renglon_caja.partner_id.id,
+                            'ref': ref + ' - Cancelación',
+                            'account_payment_id': renglon_caja.account_payment_id.id,
+                            'box_session_journal_id': box_session_journal_id.id
+                        }
+
+                        self.env['box.session.journal.line'].create(vals)
                     else:
-                        ref = ''
-
-                    vals = {
-                        'name': renglon_caja.display_name, 
-                        'amount': -renglon_caja.amount, 
-                        'partner_id': renglon_caja.partner_id.id,
-                        'ref': ref + ' - Cancelación',
-                        'account_payment_id': renglon_caja.account_payment_id.id,
-                        'box_session_journal_id': box_session_journal_id.id
-                    }
-
-                    self.env['box.session.journal.line'].create(vals)
-                else:
-                    print("No tiene que anular en la caja 1.")
+                        print("No tiene que anular en la caja 1.")
+                    
             else:
                 print("No tiene que anular en la caja 2.")
