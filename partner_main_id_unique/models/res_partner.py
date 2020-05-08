87860a981d86b1031f8ae58b197eedda3e16f0ab
Juan P Garza <juanp@juanpgarza.com>
2020-05-08 19:47:31 -0300

    Ajustes remito impreso:     tamaño letra     error más de una factura     Nro remito del pie, correr a la derecha Error control de CUIT repetido cuando quedaba sin informar Quitar columnas sobrante vista lista de módulo tarjetas

--- partner_main_id_unique/models/res_partner.py
+++ partner_main_id_unique/models/res_partner.py
@@ -9,18 +9,13 @@ class ResPartner(models.Model):
     @api.constrains('main_id_number')
     def _check_main_id_number_unique(self):
         for record in self:
-            # if record.parent_id or not record.vat:
-            #     continue
-            # test_condition = (config['test_enable'] and
-            #                   not self.env.context.get('test_vat'))
-            # if test_condition:
-            #     continue
-            results = self.env['res.partner'].search_count([
-                ('parent_id', '=', False),
-                ('main_id_number', '=', record.main_id_number),
-                ('id', '!=', record.id)
-            ])
-            if results:
-                raise ValidationError(_(
-                    "El número de identificación %s ya existe en otro "
-                    "contacto.") % record.main_id_number)
+            if record.main_id_number:
+                results = self.env['res.partner'].search_count([
+                    ('parent_id', '=', False),
+                    ('main_id_number', '=', record.main_id_number),
+                    ('id', '!=', record.id)
+                ])
+                if results:
+                    raise ValidationError(_(
+                        "El número de identificación %s ya existe en otro "
+                        "contacto.") % record.main_id_number)
