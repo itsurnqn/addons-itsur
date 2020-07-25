796245ed03cbe9e987e05b9d083f706479a83a7c
Juan P Garza <juanp@juanpgarza.com>
2020-07-25 11:50:40 -0300

    sale_order_margin_percent_ux

---
+++ sale_order_margin_percent_ux/models/sale_order_margin.py
@@ -0,0 +1,25 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, api, fields
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    percent = fields.Float(
+        string='Percent',
+        compute='_compute_percent',
+        digits=(16, 2),
+        )
+
+    @api.depends('margin', 'amount_untaxed')
+    def _compute_percent(self):
+        for order in self:
+            purchase_price_total = 0
+            for line in order.order_line:
+                purchase_price_total += line.purchase_price
+            if order.margin and purchase_price_total:
+                # caso Pronto
+                order.percent = (order.margin / purchase_price_total) * 100
+                # como lo hace OCA
+                # order.percent = (order.margin / order.amount_untaxed) * 100
