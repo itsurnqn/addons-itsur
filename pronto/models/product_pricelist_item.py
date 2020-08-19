9ef153eab37a6ae2d62b67d8566ed859871b3545
Juan P Garza <juanp@juanpgarza.com>
2020-08-19 17:39:41 -0300

    [BUG] bloquea categorias que no estan duplicadas

--- pronto/models/product_pricelist_item.py
+++ pronto/models/product_pricelist_item.py
@@ -8,23 +8,26 @@ from odoo.exceptions import ValidationError
 class ProductPricelistItem(models.Model):
     _inherit = 'product.pricelist.item'
 
-    @api.constrains('product_tmpl_id','pricelist_id')
+    @api.constrains('product_tmpl_id','pricelist_id','categ_id')
     def _verificar_duplicados(self):
         if not self.id:
             return
 
-        item = self.env['product.pricelist.item'].search([('product_tmpl_id','=',self.product_tmpl_id.id),
-                                                        ('pricelist_id','=',self.pricelist_id.id),
-                                                        ('id','!=',self.id)])
-        if item:
-            descripcion = ""
+        descripcion = ""
+        if self.applied_on == '1_product':
+            item = self.env['product.pricelist.item'].search([('product_tmpl_id','=',self.product_tmpl_id.id),
+                                                            ('pricelist_id','=',self.pricelist_id.id),
+                                                            ('id','!=',self.id)])
+            descripcion = "producto"
             
-            if self.applied_on == '1_product':
-                descripcion = "producto"
-            elif self.applied_on == '2_product_category':                
-                descripcion = "categoría"
-            else:
-                descripcion = "elemento"
+        elif self.applied_on == '2_product_category':
+            item = self.env['product.pricelist.item'].search([('categ_id','=',self.categ_id.id),
+                                                            ('pricelist_id','=',self.pricelist_id.id),
+                                                            ('id','!=',self.id)])
+            descripcion = "categoría"            
+        else:
+            item = False
 
+        if item:
             raise ValidationError("Este {} ya tiene precio asignado en esta lista ".format(descripcion))
             # self.env.user.notify_danger(message="Este %s ya tiene precio asignado en esta lista " % descripcion)        
