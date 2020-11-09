495943dc0654493b50c7e16ecb3203cf6f7bfe7f
Juan P Garza <juanp@juanpgarza.com>
2020-11-09 19:15:24 -0300

    [12.0][FIX] pronto: el campo costo del producto se actualiza apenas cambia el costo en la lista de precios

--- pronto/models/product_pricelist_item.py
+++ pronto/models/product_pricelist_item.py
@@ -31,3 +31,19 @@ class ProductPricelistItem(models.Model):
         if item:
             raise ValidationError("Este {} ya tiene precio asignado en esta lista ".format(descripcion))
             # self.env.user.notify_danger(message="Este %s ya tiene precio asignado en esta lista " % descripcion)        
+
+    @api.multi
+    def write(self, values):
+        res = super(ProductPricelistItem,self).write(values)
+        if self.pricelist_id == self.env.user.company_id.product_pricelist_cost_id and self.product_tmpl_id:
+            self.product_tmpl_id._actualizar_costo(self.product_tmpl_id.id)
+
+        return res
+       
+    @api.model
+    def create(self,values):
+        res = super(ProductPricelistItem,self).create(values)
+        if res.pricelist_id == self.env.user.company_id.product_pricelist_cost_id and res.product_tmpl_id:
+            res.product_tmpl_id._actualizar_costo(res.product_tmpl_id.id)            
+
+        return res    
