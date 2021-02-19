3a9a42e81e028b53a3872d617af067ba746c7673
Juan P Garza <juanp@juanpgarza.com>
2021-02-19 16:02:38 -0300

    [12.0][IMP] pronto: buscar por tarifa en contacto

--- pronto/models/res_partner.py
+++ pronto/models/res_partner.py
@@ -15,3 +15,8 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     tipo_cliente_id = fields.Many2one('sale.tipo.cliente',string='Tipo Cliente', ondelete='Restrict')
+
+    property_product_pricelist = fields.Many2one(
+        'product.pricelist', 'Pricelist', compute='_compute_product_pricelist',
+        inverse="_inverse_product_pricelist", company_dependent=False, store=True,
+        help="This pricelist will be used, instead of the default one, for sales to the current partner")
