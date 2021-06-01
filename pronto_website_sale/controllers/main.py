e128ad7f1af27e7907e6b4ec31dd22736d3c79e5
Juan P Garza <juanp@juanpgarza.com>
2021-06-01 11:21:50 -0300

    [12.0][FIX] pronto_website_sale: error de permisos. faltaba sudo()

--- pronto_website_sale/controllers/main.py
+++ pronto_website_sale/controllers/main.py
@@ -39,12 +39,12 @@ class ProntoWebsiteSale(WebsiteSale):
     def checkout_form_validate(self, mode, all_form_values, data):
         error, error_message = super().checkout_form_validate(
             mode=mode, all_form_values=all_form_values, data=data)
-        write_error, write_message = \
-            request.env['res.partner'].sudo().try_write_commercial(
-                all_form_values)
-        if write_error:
-            error.update(write_error)
-            error_message.extend(write_message)
+        # write_error, write_message = \
+        #     request.env['res.partner'].sudo().try_write_commercial(
+        #         all_form_values)
+        # if write_error:
+        #     error.update(write_error)
+        #     error_message.extend(write_message)
 
         # tabular ciudades
         zip = all_form_values['zip']
@@ -56,14 +56,14 @@ class ProntoWebsiteSale(WebsiteSale):
         return error, error_message
 
     def _checkout_form_save(self, mode, checkout, all_values):
-        if all_values.get('commercial_partner_id', False):
-            commercial_fields = [
-                'main_id_number', 'main_id_category_id',
-                'afip_responsability_type_id']
-            for item in commercial_fields:                
-                value = all_values.pop(item, False)
-                if value:
-                    checkout[item] = value
+        # if all_values.get('commercial_partner_id', False):
+            # commercial_fields = [
+            #     'main_id_number', 'main_id_category_id',
+            #     'afip_responsability_type_id']
+            # for item in commercial_fields:                
+            #     value = all_values.pop(item, False)
+            #     if value:
+            #         checkout[item] = value
         res = super()._checkout_form_save(
             mode=mode, checkout=checkout, all_values=all_values)
 
@@ -75,8 +75,8 @@ class ProntoWebsiteSale(WebsiteSale):
 
         # tipo_cliente_id = request.env.ref('_pronto.usuario_final')
         # sale_type = request.env.ref('_pronto.contado')
-        public_partner_id = request.env.ref('base.public_partner')
-
+        public_partner_id = request.env.ref('base.public_partner').sudo()
+        # import pdb; pdb.set_trace()
         values = {
             # 'tipo_cliente_id': tipo_cliente_id.id,
             'tipo_cliente_id': public_partner_id.tipo_cliente_id.id,
@@ -90,11 +90,42 @@ class ProntoWebsiteSale(WebsiteSale):
             'country_id': zip_id.city_id.country_id.id,
         }
 
-        partner_id.sudo().write(values)
+        partner_id.with_context(create_from_website=True).sudo().write(values)
 
         order = request.website.sale_get_order()
         order.commitment_date = order.expected_date
         
+        # import pdb; pdb.set_trace()
+        ###Cliente (dirección) Facturación###
+        #1 busco cliente por:
+        # tipo doc, nro doc y tipo resp Afip
+        if all_values.get('commercial_partner_id', False):
+            # partner_id_facturacion = request.env["res.partner"].sudo().search(
+            #     [('main_id_number','=',all_values['main_id_number']),
+            #     ('main_id_category_id','=',all_values['main_id_category_id']),
+            #     ('afip_responsability_type_id','=',all_values['afip_responsability_type_id'])]
+            #     )
+
+            partner_id_facturacion = request.env["res.partner"].sudo().search(
+                [('main_id_number','=',all_values['main_id_number'])]
+                )
+
+            # import pdb; pdb.set_trace()
+            #2 si no existe, lo tengo que crear
+            if not partner_id_facturacion:
+                values_partner_facturacion = {
+                    'name': all_values['main_id_number'],
+                    'main_id_number': all_values['main_id_number'],
+                    'main_id_category_id': all_values['main_id_category_id'],
+                    'afip_responsability_type_id': all_values['afip_responsability_type_id'],
+                }
+
+                partner_id_facturacion = request.env["res.partner"].with_context(create_from_website=True).sudo().create(values_partner_facturacion)
+
+            #3 le asigno la dir de facturacion a la sale.order
+            # import pdb; pdb.set_trace()
+            order.partner_invoice_id = partner_id_facturacion.id
+
         return res
 
     @route()
