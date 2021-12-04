c7e0e83b6254fa5e6a75fd5d26c8d00e7fdd2d6b
Juan Pablo Garza <juanp@juanpgarza.com>
2021-12-04 16:00:38 -0300

    ANULO -> [12.0][IMP] t714 pronto: cambiar depósito de devolución

--- pronto/wizards/stock_return_picking.py
+++ pronto/wizards/stock_return_picking.py
@@ -8,9 +8,9 @@ from odoo.exceptions import UserError
 class StockReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
-    location_id = fields.Many2one(
-        'stock.location', 'Return Location',
-        domain="[]")
+    # location_id = fields.Many2one(
+    #     'stock.location', 'Return Location',
+    #     domain="[]")
 
     reason_id = fields.Many2one(comodel_name="stock.return.picking.reason", string= 'Motivo de devolución')
     wait_replacement = fields.Boolean('Wait for vendor replacement', default=True)
